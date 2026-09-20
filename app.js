9898dcfdaf33689972fb034548ae7e525aafd5e8
amirfarsa <dev.amirfarsa@gmail.com>
2026-09-20 16:57:13 +0330

    addedvalidation

--- app.js
+++ app.js
@@ -1,6 +1,33 @@
 let userNameInput = document.querySelector('.username')
 let passwordInput = document.querySelector('.password')
 let modal = document.querySelector('.modal')
+let usernameMassage = document.querySelector('.username-validation')
+let passwordMassage = document.querySelector('.password-validation')
+
+let usernameIn = document.querySelector('.username')
+let passwordIn = document.querySelector('.password')
+
+function usernameValidation(params) {
+    if (usernameIn.value.length < 12) {
+        usernameMassage.style.color = 'red'
+        usernameMassage.innerHTML = 'باید 12 کاراکتر یا بیشتر باشد'
+        usernameMassage.style.display = 'block'
+    }else {
+        usernameMassage.style.color = 'green'
+        usernameMassage.innerHTML = 'مقدار ایمیل درست میباشد'
+    }
+}
+
+function passwordValidation(params) {
+    if (passwordIn.value.length < 8) {
+        passwordMassage.style.color = 'red'
+        passwordMassage.innerHTML = 'باید 8 کاراکتر یا بیشتر باشد'
+        passwordMassage.style.display = 'block'
+    }else {
+        passwordMassage.style.color = 'green'
+        passwordMassage.innerHTML = 'مقدار پسورد درست میباشد'
+    }
+}
 
 function dataValidation() {
     let userNameValue = userNameInput.value
